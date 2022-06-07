f29766131c785e4d5d65b7e46df1c0e6c0a74901
Andrew Porshnev <andrew.porshnev@yandex.ru>
2022-06-07 19:54:25 +0300

    Убирает max_length поля slug в модели Group

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
     def __str__(self):
