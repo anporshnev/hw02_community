9bfaaf5af3186efcb30d2541f092803efc523e93
Andrew Porshnev <andrew.porshnev@yandex.ru>
2022-06-07 19:27:24 +0300

    Убирает лишний include

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('posts.urls', namespace='index_posts')),
-    path('group/<slug:slug>/', include('posts.urls', namespace='group_posts')),
+    path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
 ]
