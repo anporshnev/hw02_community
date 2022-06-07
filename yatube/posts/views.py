67849c22bc66c8e4595305b5bda2717bb1b60fa9
Andrew Porshnev <andrew.porshnev@yandex.ru>
2022-06-07 19:40:13 +0300

    Переносит адреса темплейтов из переменной сразу в render

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,18 +4,16 @@ from .models import Post, Group
 
 
 def index(request):
-    template = 'posts/index.html'
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {'posts': posts}
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
         'group': group,
         'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
